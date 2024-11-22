c9179cb7d639edbeb574ccbccc3e95e0280d059c
alianjum-web <muhammadalikhanzada7890@gmail.com>
2024-11-22 08:01:33 +0500

    Register page work and send the token and store it.Modify the navbar so that if the user id register than not show him the register button show him the username and logout button

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,82 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
 
-import './RegisterPage.css';
+export default function Navbar() {
+    const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
 
-function RegisterPage() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate=useNavigate();
+    useEffect(() => {
+        const authTokenFromSession = sessionStorage.getItem('auth-token');
+        const nameFromSession = sessionStorage.getItem('name');
+        if (authTokenFromSession) {
+            if(isLoggedIn && nameFromSession) {
+              setUserName(nameFromSession);
+            } else {
+              sessionStorage.removeItem('auth-token');
+              sessionStorage.removeItem('name');
+              sessionStorage.removeItem('email');
+              setIsLoggedIn(false);
+            }
+        }
+    },[isLoggedIn, setIsLoggedIn, setUserName])
+    const handleLogout=()=>{
+        sessionStorage.removeItem('auth-token');
+        sessionStorage.removeItem('name');
+        sessionStorage.removeItem('email');
+        setIsLoggedIn(false);
+        navigate(`/app`);
 
-    const handleRegister = async () => {
-        console.log("Register invoked")
     }
+    const profileSecton=()=>{
+      navigate(`/app/profile`);
+    }
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
+        <a className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</a>
 
-return (
-    <div className="container mt-5">
-        <div className="row justify-content-center">
-            <div className="col-md-6 col-lg-4">
-                <div className="register-card p-4 border rounded">
-                    <h2 className="text-center mb-4 font-weight-bold">Register</h2>
-                    <div className="mb-3">
-                        <label htmlFor="firstName" className="form-label">FirstName</label>
-                        <input
-                            id="firstName"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your firstName"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
-                    </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                    {/* last name */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/app">Gifts</Link> {/* Updated Link */}
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/app/search">Search</Link>
+            </li>
+            <ul className="navbar-nav ml-auto">
+            {isLoggedIn ? (
+                                    <>
+                                    <li className="nav-item"> <span className="nav-link" style={{color: "black", cursor:"pointer"}} onClick={profileSecton}>Welcome, {userName}</span> </li>
+                                    <li className="nav-item"><button className="nav-link login-btn" onClick={handleLogout}>Logout</button></li>
+                                    </>
+                                    )  : (
+                                    <>
+                                        <li className="nav-item">
 
-                    <div className="mb-3">
-                        <label htmlFor="lastName" className="form-label">LastName</label>
-                        <input
-                            id="lastName"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
+                                        <Link className="nav-link login-btn" to="/app/login">Login</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                        <Link className="nav-link register-btn" to="/app/register">Register</Link>
+                                        </li>
+                                    </>
+                                )
 
-                    {/* email  */}
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            id="email"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="mb-4">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input
-                            id="password"
-                            type="password"
-                            className="form-control"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
-                    <p className="mt-4 text-center">
-                        Already a member? <a href="/app/login" className="text-primary">Login</a>
-                    </p>
-                </div>
-            </div>
+                                }
+            </ul>
+          </ul>
         </div>
-    </div>
-);
+      </nav>
+        </>
+    )
 }
-
-export default RegisterPage;
